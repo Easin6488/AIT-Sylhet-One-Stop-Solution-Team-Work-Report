a4ce9af7a6459d24740dd5a69a783cd47a320907
Easin6488 <easin.amberit@gmail.com>
2026-09-22 19:48:01 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API_URL="https://script.google.com/macros/s/AKfycbxw5_5TTSFLzhP4sgOTfEsvvNCQIauBdMeq_nlabGQlM7aAtI1RRmbv4lfgt5Gb65Sq/exec";
 
-const GOOGLE_CLIENT_ID="846667579173-95git7niukirumrlpbo084uh7fev85ro.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID="846667579173-95git7niukirumrlpbo084uh7fev85ro.apps.googleusercontent.com.apps.googleusercontent.com";
 
 const AUTO_REFRESH_MS=15000;
